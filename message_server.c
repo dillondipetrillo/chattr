e69cbf0f6f78d442c7c0a8f40001e3d62e680647
dillondipetrillo <dillonrdipetrillo@gmail.com>
2026-03-11 20:45:06 -0400

    refactor setting up the server socket into its own function

--- message_server.c
+++ message_server.c
@@ -7,37 +7,14 @@
 
 #define MAX_BUFF_SIZE 1024
 
+int setup_server(void);
+
 int main(void)
 {
-    int server = socket(PF_INET, SOCK_STREAM, 0);
-    if (server == -1) {
-        printf("Error: Cannot create socket.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
-
-    address.sin_family = AF_INET;
-    address.sin_port = htons(8080);
-    address.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(server,
-        (struct sockaddr *) &address,
-        sizeof(address)) == -1
-    ) {
-        printf("Error: Cannot bind socket to address.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server, 4) == -1) {
-        printf("Error: Cannot start listening.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int client_socket = accept(server, NULL, NULL);
+    int server_socket = setup_server();
+    int client_socket = accept(server_socket, NULL, NULL);
     if (client_socket == -1) {
-        printf("Error: Cannot accept incoming client.\n");
+        perror("Error: Cannot accept incoming client.\n");
         exit(EXIT_FAILURE);
     }
     printf("Connected to client...\n");
@@ -58,8 +35,38 @@ int main(void)
         printf("Error: Socket couldn't receieve stream.\n");
     }
 
-    close(server);
+    close(server_socket);
     close(client_socket);
 
     exit(EXIT_SUCCESS);
+}
+
+int setup_server(void) {
+    int server = socket(PF_INET, SOCK_STREAM, 0);
+    if (server == -1) {
+        perror("Error: Cannot create socket.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(8080);
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(server,
+        (struct sockaddr *) &address,
+        sizeof(address)) == -1
+    ) {
+        perror("Error: Cannot bind socket to address.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server, 4) == -1) {
+        perror("Error: Cannot start listening.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return server;
 }
